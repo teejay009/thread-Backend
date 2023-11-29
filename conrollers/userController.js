b7740f31ba3bd42cdb054075c0ddee38031bd458
teejay009 <tajdjimoh@gmail.com>
2023-11-29 15:19:02 +0100

    update

--- conrollers/userController.js
+++ conrollers/userController.js
@@ -12,7 +12,7 @@ const getUserProfile = async (req, res) => {
         let user;
 
         //  query is userId
-        if(mongoose.Types.ObjectId.isValid)(query)
+        if(mongoose.Types.ObjectId.isValid(query))
     }
 
 }     
