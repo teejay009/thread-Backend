3b57cdd30d8c941828f3b880ecfadd173808eb56
teejay009 <tajdjimoh@gmail.com>
2023-11-29 15:03:46 +0100

    update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,15 +1,16 @@
 const express = require ('express')
-const { followUnFollowUser, signUpUser, loginUser, logoutUser } = require ("../conrollers/userController");
+const { getUserProfile, followUnFollowUser, signUpUser, loginUser, logoutUser } = require ("../conrollers/userController");
 const protectRoute = require('../middleware/protectRoute');
-const getUserProfile = require() 
 
 
 const router = express.Router();
 
-router.get('/')
+router.get('/profile/:query', getUserProfile)
 router.post('/signup', signUpUser)
 router.post('/login', loginUser)
 router.post('/logout', logoutUser)
 router.post('/follow/:id', protectRoute, followUnFollowUser) // toggle state(follow/unfollow)
 
 module.exports= router;
+
+
