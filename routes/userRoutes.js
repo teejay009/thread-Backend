2c7631fef2e65954bf3dd26611b0afb00af6388f
teejay009 <tajdjimoh@gmail.com>
2023-11-29 11:35:30 +0100

    update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,6 @@ const router = express.Router();
 
 router.post('/signup', signUpUser)
 router.post('/login', loginUser)
-router.post('/logout', logoutUser)
+router.get('/logout', logoutUser)
 
 module.exports= router;
