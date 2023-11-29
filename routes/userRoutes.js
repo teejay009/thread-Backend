5c5b8877f4c758b53312afa7f18674350b790f5c
teejay009 <tajdjimoh@gmail.com>
2023-11-29 14:59:56 +0100

    update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,10 +6,10 @@ const getUserProfile = require()
 
 const router = express.Router();
 
+router.get('/')
 router.post('/signup', signUpUser)
 router.post('/login', loginUser)
 router.post('/logout', logoutUser)
 router.post('/follow/:id', protectRoute, followUnFollowUser) // toggle state(follow/unfollow)
-router.get('/')
 
 module.exports= router;
