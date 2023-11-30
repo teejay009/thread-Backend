cb9f3675639e155afc98932d1d6d1642f84ee5a5
teejay009 <tajdjimoh@gmail.com>
2023-11-30 09:35:02 +0100

    update

--- conrollers/userController.js
+++ conrollers/userController.js
@@ -154,7 +154,23 @@ const updateUser = async (req, res) => {
     const {name, email, username, password, profilePic, bio} = req.body
     const userId = req.user._id
     try {
-        
+        let user = await User.findById(userId)
+        if(!user) return res.status(400).json({mesage: "User not found"});
+
+        if (password) {
+            const salt = await bcrypt.genSalt(10)
+            const hashedPassword = await bcrypt.hash(password, salt)
+            user.password = hashedPassword
+        }
+
+        user.name = name || user.name
+        user.email = email || user.email
+        user.username = username || user.username
+        user.profilePic = profilePic || user.profilePic
+        user.bio = bio || user.bio
+
+        user = await user.save()
+        res.status(200).json({message: "Profile Updated Suggessfully", user})
        
     } catch (err) {
 		res.status(500).json({ message: err.message }); //Internal server error
