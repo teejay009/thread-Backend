f950ec6dc68189fda3445e2ac757833520e2bdd5
teejay009 <tajdjimoh@gmail.com>
2023-11-30 09:15:00 +0100

    update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,7 +13,7 @@ router.post('/signup', signUpUser)
 router.post('/login', loginUser)
 router.post('/logout', logoutUser)
 router.post('/follow/:id', protectRoute, followUnFollowUser) // toggle state(follow/unfollow)
-router.post('/update/:id', protectRoute, updateUser)
+router.post('/update/:id', protectRoute, updateUser) //
 
 module.exports= router;
 
