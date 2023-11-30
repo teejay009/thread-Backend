394fff9cfb57a4c0c59f06ce06423bb9fba0a099
teejay009 <tajdjimoh@gmail.com>
2023-11-30 09:54:04 +0100

    update

--- conrollers/userController.js
+++ conrollers/userController.js
@@ -170,7 +170,7 @@ const updateUser = async (req, res) => {
         user.bio = bio || user.bio
 
         user = await user.save()
-        res.status(200).json({message: "Profile Updated Suggessfully", user})
+        res.status(200).json({message: "Profile Updated Successfully", user})
        
     } catch (err) {
 		res.status(500).json({ message: err.message }); //Internal server error
